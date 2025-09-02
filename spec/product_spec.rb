e4291831a14d2509d3b2fdf6456f8f93eaf16819
Levii01 <m.sedziak01@gmail.com>
2025-09-02 15:05:12 +0200

    Fix product spec

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,7 +3,7 @@
 require_relative '../lib/product'
 
 RSpec.describe Product do
-  subject(:product) { described_class.new('CF1', 11.23, 'Coffee') }
+  subject(:product) { described_class.new(code: 'CF1', price: 11.23, name: 'Coffee') }
 
   it 'initializes with code, price, and name' do
     expect(product).to have_attributes(code: 'CF1', price: 11.23, name: 'Coffee')
